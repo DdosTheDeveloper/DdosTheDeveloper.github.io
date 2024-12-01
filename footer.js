d5621e59b45ecbea5e6e7ca9e17f4e25277806f4
DdosTheDeveloper <ddos_69@tutamail.com>
2024-12-01 14:34:12 +0100

    Add files via upload

---
+++ footer.js
@@ -0,0 +1,28 @@
+export function createFooter() {
+    document.addEventListener("DOMContentLoaded", function () {
+      document.body.insertAdjacentHTML(
+        "beforeend",
+        `
+        <footer class="footer">
+          <div class="footer-content">
+            <div class="footer-logo">
+              <img src="Strong-Media-logo-v5.png" alt="Strong Media Logo">
+            </div>
+            <div class="footer-contact">
+              <h3>Контакти</h3>
+              <p>Імейл: support@example.com</p>
+              <p>Телефон: +380 99 60 94 748</p>
+            </div>
+            <div class="footer-social">
+              <h3>Соціальні мережі</h3>
+              <a href="https://www.instagram.com" target="_blank">Інстаграм</a>
+              <a href="https://www.facebook.com" target="_blank">Фейсбук</a>
+            </div>
+          </div>
+        </footer>
+      `
+      );
+    });
+  }
+  
+  createFooter();
--- menu.js
+++ menu.js
@@ -5,7 +5,12 @@ export function createTopMenu() {
       `
       <div class="top-menu">
         <ul>
-          <li><a href="#" data-page="home">Home</a></li>
+          <li>
+            <a href="#" data-page="home">
+              <img src="Strong-Media-logo-v4.png" class="logo" alt="Home">
+            </a>
+          </li>
+          <li><a href="#" data-page="about-us">Про нас</a></li>
           <li><a href="#" data-page="contact">Контакти</a></li>
         </ul>
       </div>
@@ -24,11 +29,15 @@ export function createTopMenu() {
 }
 
 function loadPage(page) {
-  // Define the content for each page
   const pages = {
     home: `
       <h1>Розширення спортзалу завдяки соціальним мережам - це наша спеціалізація</h1>
       <p>Навідмінну від конкурентів, спортзали - це наша спеціалізація</p>
+    `,
+    "about-us": `
+      <h1>Про нас</h1>
+      <p>Ми компанія, яка спеціалізується на просуванні спортзалів через соціальні мережі.</p>
+      <p>Ціль нашого бізнесу це допомогти твоєму рости.</p>
     `,
     contact: `
       <h1>Наші контакти</h1>
@@ -39,18 +48,18 @@ function loadPage(page) {
     `,
   };
 
-  // Check if the requested page exists
   if (pages[page]) {
     document.querySelector(".content").innerHTML = pages[page];
+    history.pushState({ page }, "", `#${page}`); // Add entry to history
   } else {
     document.querySelector(".content").innerHTML =
       "<p>Page not found. Please try again.</p>";
   }
 }
 
-
 window.addEventListener("popstate", (event) => {
-  const page = event.state?.page || "home";
+  const page = event.state?.page || "home"; // Default to home if no state
   loadPage(page);
-})
+});
+
 createTopMenu();
